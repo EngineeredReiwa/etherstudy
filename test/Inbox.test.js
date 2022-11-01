5b204df9bc4301c439f276f2b546949b21ae4bc8
Keisuke Kakuda <k.keisuke.1230@gmail.com>
2022-11-02 00:18:13 +0900

    47. Fetching Accounts from Ganache

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -6,25 +6,14 @@ const Web3 = require("web3");
 // Instance of web3 library
 const web3 = new Web3(ganache.provider());
 
-class Car {
-    park() {
-        return "stopped";
-    }
-    drive() {
-        return "vroom";
-    }
-}
-let car;
-
 beforeEach(() => {
-    car = new Car();
+    // Get list of all accounts
+    web3.eth.getAccounts().then((fetchedAccounts) => {
+        console.log(fetchedAccounts);
+    });
+
+    // Use one of those accounts to deploy
+    // the contract
 });
 
-describe("Car", () => {
-    it("can park", () => {
-        assert.equal(car.park(), "stopped");
-    });
-    it("can drive", () => {
-        assert.equal(car.drive(), "vroom");
-    });
-});
+describe("Inbox", () => it("deploys a contract", () => {}));
